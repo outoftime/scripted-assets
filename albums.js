44fb6143259e058824d05d8776dcdb3a6c519254
Mat Brown <mat@genius.com>
2016-05-11 21:29:52 -0400

    window.addAlbum

--- albums.js
+++ albums.js
@@ -1,4 +1,4 @@
-function addAlbum(object) {
+window.addAlbum = function(object) {
   var $container = $('<div class="album"></div>');
   var $image = $('<img>').attr('src', object.imageURL).appendTo($container);
   var $title = $('<div class="title"></div>').text(object.title).appendTo($container);
