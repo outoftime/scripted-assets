106638b843ab40efaa7c706c07fcc1f0e1b7fe48
Mat Brown <mat@genius.com>
2016-05-11 21:23:50 -0400

    Add albums

---
+++ albums.js
@@ -0,0 +1,6 @@
+function addAlbum(object) {
+  var $container = $('<div class="album"></div>');
+  var $image = $('<img>').attr('src', object.imageURL).appendTo($container);
+  var $title = $('<div class="title"></div>').text(object.title).appendTo($container);
+  var $year = $('<div class="year"></div>').text(object.year).appendTo($container);
+}
